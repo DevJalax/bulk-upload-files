a89d9e5304a6ee182ba7221f0b2c76e9b01857ed
Jalakandeshwaran <raonejalagandeswaran@gmail.com>
2024-11-16 12:16:42 +0530

    Create DocumentResponse.java

---
+++ DocumentResponse.java
@@ -0,0 +1,13 @@
+public class DocumentResponse {
+    private String fileName;
+    private String fileUrl;
+    private String fileType;
+
+    public DocumentResponse(String fileName, String fileUrl, String fileType) {
+        this.fileName = fileName;
+        this.fileUrl = fileUrl;
+        this.fileType = fileType;
+    }
+
+    // Getters and Setters
+}
